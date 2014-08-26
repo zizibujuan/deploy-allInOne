93e9fd68a927266f1131fac333457e8b9e3eb4ec
jinzw <zhengwei.jin@gmail.com>
2014-08-26 22:21:19 +0800

    测试

--- copy_project.sh
+++ copy_project.sh
@@ -9,13 +9,3 @@ targetServerBundle=$targetProject/server/bundles
 targetServerFeature=$targetProject/server/features
 targetClientBundle=$targetProject/client/bundles
 releaseClientDir=$targetClientBundle/com.zizibujuan.drip.client.release/static
-
-echo "delete old files..."
-rm -rf $targetServerBundle
-rm -rf $targetServerFeature
-rm -rf $releaseClientDir
-
-
-mkdir -p $targetServerBundle
-mkdir -p $targetServerFeature
-mkdir $targetClientBundle/com.zizibujuan.drip.client.release/static
